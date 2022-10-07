82aa3b72113825f915de75c81f8268f8ea6d0de9
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-10-07 15:21:23 -0300

    iniciando projeto

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>SUA VEZ</p>
       </header>
     </div>
   );
 }
+
+//
