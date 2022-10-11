13069fcf458d8ab4cac83e4f8d6ec724bf15d4ab
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-10-11 20:21:10 -0300

    req 17 concluido

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
 import App from '../App';
+import { questionsResponse } from '../../cypress/mocks/questions';
 
 const nameText = 'input-player-name';
 const emailText = 'input-gravatar-email';
 
 describe('testa componente Login', () => {
+
+  beforeEach(() => {
+
+    global.fetch = jest.fn(async (endPoint) => ({
+      json: async () => {
+        if (endPoint.includes('//opentdb.com/api.php?amount=5&token=')) {
+          return questionsResponse
+        }
+        return {
+          "response_code": 0,
+          "response_message": "Token Generated Successfully!",
+          "token": "f00cb469ce38726ee00a7c6836761b0a4fb808181a125dcde6d50a9f3c9127b6"
+        }
+      }
+    }))
+  })
+
   test('Se possui um titulo com nome Login', () => {
     renderWithRouterAndRedux(<App />);
     const loginText = screen.getByText('Login');
@@ -55,7 +73,7 @@ describe('testa componente Login', () => {
     expect(button).toBeEnabled();
   });
 
-  test('Se, ao clicar no botão Play redireciona para o /settings', () => {
+  test('Se, ao clicar no botão Configurações redireciona para o /settings', () => {
     const { history } = renderWithRouterAndRedux(<App />);
     const emailInput = screen.getByTestId(emailText);
     const nameInput = screen.getByTestId(nameText);
@@ -69,7 +87,8 @@ describe('testa componente Login', () => {
     expect(pathname).toBe('/settings');
   });
 
-  test('Se, ao clicar no botão Settings redireciona para o /game', () => {
+  test('Se, ao clicar no botão Play redireciona para o /game', async () => {
+
     const { history } = renderWithRouterAndRedux(<App />);
     const emailInput = screen.getByTestId(emailText);
     const nameInput = screen.getByTestId(nameText);
@@ -79,7 +98,8 @@ describe('testa componente Login', () => {
     userEvent.type(nameInput, '123456');
     userEvent.click(button);
 
-    const { pathname } = history.location;
-    expect(pathname).toBe('/game');
+    await waitFor(() => expect(history.location.pathname).toBe('/game')
+    )
   });
 });
+
