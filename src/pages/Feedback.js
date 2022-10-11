9701b44bb831834549bd9bdf073142b36c832a91
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-10-11 16:47:30 -0300

    requisito 10 e 11

---
+++ src/pages/Feedback.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Feedback extends React.Component {
+  render() {
+    return (
+      <div data-testid="feedback-text">Feedback</div>
+    );
+  }
+}
